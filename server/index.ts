72be681fd0e38de621bc4299db189f553f853708
Marko <marko.xmas@protonmail.com>
2024-03-19 16:32:46 +0100

    Added server to the app

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+// index.ts
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
