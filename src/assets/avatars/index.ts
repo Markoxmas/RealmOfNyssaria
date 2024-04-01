a45f2dad11c0238e01762bb8dcd2755559887b70
Marko <marko.xmas@protonmail.com>
2024-04-01 17:43:13 +0200

    Added avatar images

---
+++ src/assets/avatars/index.ts
@@ -0,0 +1,43 @@
+import Alina from "./Alina.jpg";
+import Ballmak from "./Ballmak.jpg";
+import Ciliren from "./Ciliren.jpg";
+import Cora from "./Cora.jpg";
+import Elissa from "./Elissa.jpg";
+import Eneus from "./Eneus.jpg";
+import Exon from "./Exon.jpg";
+import Ivaras from "./Ivaras.jpg";
+import LiaCigniss from "./LiaCigniss.jpg";
+import Medea from "./Medea.jpg";
+import Nuala from "./Nuala.jpg";
+import Orakh from "./Orakh.jpg";
+import Owix from "./Owix.jpg";
+import Paragon from "./Paragon.jpg";
+import Pikinea from "./Pikinea.jpg";
+import Rawena from "./Rawena.jpg";
+import Sapphire from "./Sapphire.jpg";
+import Shamus from "./Shamus.jpg";
+import Uxeor from "./Uxeor.jpg";
+import Zaleria from "./Zaleria.jpg";
+
+export const AVATARS = {
+  Alina,
+  Ballmak,
+  Ciliren,
+  Cora,
+  Elissa,
+  Eneus,
+  Exon,
+  Ivaras,
+  LiaCigniss,
+  Medea,
+  Nuala,
+  Orakh,
+  Owix,
+  Paragon,
+  Pikinea,
+  Rawena,
+  Sapphire,
+  Shamus,
+  Uxeor,
+  Zaleria,
+};
