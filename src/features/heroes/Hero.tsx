b2ef4d5d14e1ced9f86171bb9a8fd16f0b9543f3
Marko <marko.xmas@protonmail.com>
2024-04-01 22:23:19 +0200

    Added stars to hero card

--- src/features/heroes/Hero.tsx
+++ src/features/heroes/Hero.tsx
@@ -5,10 +5,29 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Hero as HeroType } from "./heroesSlice";
 import { AVATARS } from "../../assets/avatars/index";
+import StarIcon from "@mui/icons-material/Star";
+
+function renderStars(hero: HeroType) {
+  let stars = [];
+  const starsAmount = hero.stars > 5 ? hero.stars - 5 : hero.stars;
+  const starsColor = hero.stars > 5 ? "red" : "yellow";
+  for (let i = 0; i < starsAmount; i++) {
+    stars.push(<StarIcon key={hero._id + i} sx={{ color: starsColor }} />);
+  }
+  return stars;
+}
 
 export default function Hero({ hero }: { hero: HeroType }) {
   return (
-    <Card sx={{ width: 200, textAlign: "center", border: "1px solid black" }}>
+    <Card
+      sx={{
+        width: 200,
+        textAlign: "center",
+        border: "1px solid black",
+        padding: "5px",
+        borderRadius: "10px",
+      }}
+    >
       <CardHeader
         title={
           <Typography variant="body1" noWrap>
@@ -23,8 +42,10 @@ export default function Hero({ hero }: { hero: HeroType }) {
         width="200"
         image={AVATARS[hero.image]}
         alt={hero.name}
+        style={{ borderRadius: "10px" }}
       />
       <CardContent>
+        {renderStars(hero)}
         <Typography variant="body2" color="text.secondary">
           CP: {hero.cp}
         </Typography>
