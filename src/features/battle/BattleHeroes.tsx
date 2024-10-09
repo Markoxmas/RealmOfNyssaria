a70f9d519645ee3187205806ae908de22b7219b6
unknown <marko.xmas@protonmail.com>
2024-10-10 00:09:53 +0200

    Simplifying battle heroes display

--- src/features/battle/BattleHeroes.tsx
+++ src/features/battle/BattleHeroes.tsx
@@ -16,56 +16,44 @@ function renderStars(hero: Hero) {
 
 export default function BattleHeroes() {
   const { battle } = useAppSelector((state) => state.battle);
-  const { heroes } = useAppSelector((state) => state.heroes);
-  const heroesInBattle: Hero[] = [];
-
-  if (battle) {
-    const lastMilestone =
-      battle.battleMilestones[battle.battleMilestones.length - 1];
-    lastMilestone.heroes.forEach((hero) => {
-      const foundHero = heroes.find(
-        (inventoryHero) => hero._id === inventoryHero._id
-      );
-      if (foundHero) {
-        heroesInBattle.push(foundHero);
-      }
-    });
-  }
 
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
-      {heroesInBattle.map((hero) => {
-        return (
-          <Card
-            sx={{
-              width: 150,
-              textAlign: "center",
-              border: "3px solid rgba(189, 195, 199, 1)",
-              padding: "2px",
-              borderRadius: "10px",
-              background: "rgba(218, 223, 225, 1)",
-            }}
-          >
-            <div>
-              <b>
-                {hero.name} [{hero.level}]
-              </b>
-            </div>
-            <CardMedia
-              component="img"
-              height="150"
-              width="150"
-              image={AVATARS[hero.image]}
-              alt={hero.name}
-              style={{ borderRadius: "10px" }}
-            />
-            <div>
-              <div>{hero.cp} CP</div>
-              {renderStars(hero)}
-            </div>
-          </Card>
-        );
-      })}
+      {battle?.battleMilestones?.length &&
+        battle.battleMilestones[battle.battleMilestones.length - 1].heroes.map(
+          (hero) => {
+            return (
+              <Card
+                sx={{
+                  width: 150,
+                  textAlign: "center",
+                  border: "3px solid rgba(189, 195, 199, 1)",
+                  padding: "2px",
+                  borderRadius: "10px",
+                  background: "rgba(218, 223, 225, 1)",
+                }}
+              >
+                <div>
+                  <b>
+                    {hero.name} [{hero.level}]
+                  </b>
+                </div>
+                <CardMedia
+                  component="img"
+                  height="150"
+                  width="150"
+                  image={AVATARS[hero.image]}
+                  alt={hero.name}
+                  style={{ borderRadius: "10px" }}
+                />
+                <div>
+                  <div>{hero.cp} CP</div>
+                  {renderStars(hero)}
+                </div>
+              </Card>
+            );
+          }
+        )}
     </div>
   );
 }
