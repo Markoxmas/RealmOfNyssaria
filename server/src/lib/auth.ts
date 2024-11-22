53867d95034837bbbae33b1a756dbd90af9cfb02
unknown <marko.xmas@protonmail.com>
2024-11-22 19:54:18 +0100

    Increase token validity to 1 week

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -10,7 +10,7 @@ const generateToken = (res: Response, userId: string) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     sameSite: "strict",
-    maxAge: 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
