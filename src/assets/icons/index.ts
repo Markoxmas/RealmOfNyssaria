ce77691ab0e7bf2106a1a554453391c45191e3ee
unknown <marko.xmas@protonmail.com>
2024-11-24 01:27:24 +0100

    Added some icons for drops

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,8 @@
 import hero_icon from "./hero_icon.png";
 import scroll_of_summon from "./scroll_of_summon.png";
 import gold from "./gold.png";
+import rock from "./rock.png";
+import test_weapon from "./test_weapon.png";
 
 interface Icon {
   [key: string]: string;
@@ -10,4 +12,6 @@ export const ICONS: Icon = {
   hero_icon,
   scroll_of_summon,
   gold,
+  rock,
+  test_weapon,
 };
